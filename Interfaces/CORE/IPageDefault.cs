3a0d09560543f1884a92e571c8d0275a460b6e78
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2025-02-27 15:01:21 +0300

    Обновление реализации страницы

--- Interfaces/CORE/IPageDefault.cs
+++ Interfaces/CORE/IPageDefault.cs
@@ -1,9 +1,13 @@
 ﻿using IEL.Classes;
+using System.Windows.Controls;
 
 namespace IEL.Interfaces.Core
 {
     public interface IPageDefault : IPage
     {
-
+        /// <summary>
+        /// Объект страницы распологающий элементы
+        /// </summary>
+        public Page Content { get; }
     }
 }
