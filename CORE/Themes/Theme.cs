b8f60dcc02dca2082bbef5aefbd720b69da17af7
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2026-07-27 19:27:09 +0300

    Обновление предостановления данных для реалищации персонализации

---
+++ CORE/Themes/Theme.cs
@@ -0,0 +1,39 @@
+﻿using IEL.CORE.Classes;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace IEL.CORE.Themes
+{
+    /// <summary>
+    /// Главный класс управления темами
+    /// </summary>
+    public static class Theme
+    {
+        /// <summary>
+        /// Расширение файлов тем
+        /// </summary>
+        public static readonly string ExtensionThemeFile = ".qd";
+
+        /// <summary>
+        /// Числовой тип, который используется для перечисления спектров палитры
+        /// </summary>
+        public static readonly Type EnumUnderlyingTypePalette = typeof(uint);
+
+        /// <summary>
+        /// Узнать тип перечисления для спектров палитры
+        /// </summary>
+        /// <param name="NameType">Имя поискового типа</param>
+        /// <param name="SourceAssembly">Сборка в которой хранится тип</param>
+        public static Type? GetEnumSpectrumType(Assembly SourceAssembly, string NameType)
+        {
+            Type[] AllTypesCallAssembly = SourceAssembly.GetTypes();
+            Type? SourceType = AllTypesCallAssembly.FirstOrDefault((i) => i.Name.Equals(NameType));
+            if (SourceType == null) return null;
+            else if (SourceType.GetType().IsEnum && Enum.GetUnderlyingType(SourceType) != EnumUnderlyingTypePalette)
+                    return SourceType;
+            else return null;
+        }
+    }
+}
